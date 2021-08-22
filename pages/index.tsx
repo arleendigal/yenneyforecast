e3a819fe7f0c8359c4adb132edb195229c2d681b
Arleen Digal <yen.digal@gmail.com>
2021-08-22 19:28:14 +1000

    :tada: Yahooooo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
-import Logo from '@components/atoms/Logo'
+import FreeSolo from '@components/atoms/Freesolo'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import SimpleCard from '@components/atoms/SimpleCard'
 
 const Home: React.FC = () => {
-  return <Logo />
+  return (
+    <Container maxWidth="md">
+      <Grid container direction="row" justifyContent="center">
+        <Grid container>
+          <FreeSolo />
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <SimpleCard />
+        </Grid>
+        <Grid item xs>
+          <SimpleCard />
+        </Grid>
+        <Grid item xs>
+          <SimpleCard />
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 export default Home
