0d174f5d9d1cd41caa0009c0f5e2fa33ba5b4709
Arleen Digal <yen.digal@gmail.com>
2021-09-18 22:38:44 +1000

    connect api to search field

--- src/api/weatherService.ts
+++ src/api/weatherService.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const getSuggestedPlaces = async (query: string) => {
   return await axios.get(
-    `https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${query}`
+    `https://api.weatherapi.com/v1/search.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${query}`
   )
 }
